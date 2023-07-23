f949338992152ebab5b333e5e7d3107bc04a8ee0
Russell Bloxwich <russellbloxwich@Russells-MacBook-Pro.local>
2023-07-23 13:22:13 +1200

    hide ts error

---
+++ src/Scanner.d.ts
@@ -0,0 +1,5 @@
+import { ReactElement } from "react";
+
+declare function Scanner(props: any): JSX.Element;
+
+export = Scanner;
--- src/qr.d.ts
+++ src/qr.d.ts
@@ -1,2 +1,3 @@
 declare module "react-qr-scanner";
 declare module "@artcom/react-three-arjs";
+declare module "Scanner.js"
